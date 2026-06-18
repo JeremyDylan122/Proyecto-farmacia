18272bdffdf7d9fedbab6a271a0f1f0a6b8af5b6
jonariverag <jona.riverag@duocuc.cl>
2026-06-17 22:48:55 -0400

    Terminado el servicio de compras

--- farma/src/main/java/com/compra/farma/exception/CompraNoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.compra.farma.exception;
-
-public class CompraNoEncontradaException extends RuntimeException {
-    public CompraNoEncontradaException(String idOrdenCompra) {
-        super("Compra con id " + idOrdenCompra + " no encontrada");
-    }
-}
