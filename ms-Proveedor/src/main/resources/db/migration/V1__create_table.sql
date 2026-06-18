47b2a43c07b4d189c9269b003ccdfa2605dd8094
Jeremy <jeremyhanglin@gmail.com>
2026-06-18 18:04:27 -0400

    Final espero...

--- ms-Proveedor/src/main/resources/db/migration/V1__create_table.sql
+++ ms-Proveedor/src/main/resources/db/migration/V1__create_table.sql
@@ -1,9 +1,9 @@
+CREATE SCHEMA IF NOT EXISTS esquema_proveedor;
 
-CREATE TABLE proveedor (
+CREATE TABLE IF NOT EXISTS proveedor (
     rut_proveedor VARCHAR(15) PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     direccion VARCHAR(100) NULL,
     telefono VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
-
 );
--- ms-catalogo/src/test/java/com/producto/mscatalogo/MsCatalogoApplicationTests.java
+++ ms-catalogo/src/test/java/com/producto/mscatalogo/MsCatalogoApplicationTests.java
@@ -1,13 +1,9 @@
 package com.producto.mscatalogo;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsCatalogoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
